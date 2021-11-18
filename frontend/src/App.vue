bd86590827179eba8d3f06b65ee03143c4e2c14a
charlie <charlieputh.jjh@gmail.com>
2021-11-18 10:18:13 +0800

    update App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,33 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view v-if="showView" />
   </div>
 </template>
 
 <script>
+import { ls } from '@/utils';
+
 export default {
-  name: 'App'
-}
+  name: 'App',
+  data() {
+    return { showView: true };
+  },
+  provide() {
+    return { appReload: this.reload };
+  },
+  created() {
+    const networkType = ls.fetch('network_type');
+    if (!networkType) {
+      ls.save('network_type', 'emulator');
+    }
+  },
+  methods: {
+    reload() {
+      this.showView = false;
+      setTimeout(() => {
+        this.showView = true;
+      }, 1000 / 60);
+    }
+  }
+};
 </script>
